c4aca3bc9a567d81d38d9474da22b1b92b44f4c9
Ekremteke <ekremteke.uk@gmail.com>
2022-04-18 18:25:00 +0100

    ikinci

---
+++ src/main/java/Personel.java
@@ -0,0 +1,17 @@
+public class Personel {
+    String isim;
+
+    int idNum;
+
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+
+    public void String toString() {
+        return"personel{" +
+                "isim'" + isim + ''
+    }
+
+
+}
