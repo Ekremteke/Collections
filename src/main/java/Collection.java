2d0eaf9b0e86b04549d2e9dfdc1e82dbe3212735
Ekremteke <ekremteke.uk@gmail.com>
2022-04-18 18:51:49 +0100

    dorduncu

--- src/main/java/Collection.java
+++ src/main/java/Collection.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Collection extends Personel {
     public static void main(String[] args) {
         System.out.println("collectionslari ogreniyoruz");
@@ -9,6 +11,16 @@ Personel p1 = new Personel ();
 
         p1.setIsim("Mehmet");
         p1.setIdNum(54321);
+
+        Ogrenci ogr = new Ogrenci();
+        ogr.setName("Ekrem");
+        ogr.setDers("JAva");
+
+
+        ArrayList<Personel> ilkList = new ArrayList<>();
+
+        ilkList.add(p);
+        ilkList.add(p1);
 
 
     }
--- src/main/java/Ogrenci.java
+++ src/main/java/Ogrenci.java
@@ -1,5 +1,20 @@
 public class Ogrenci {
-    String isim;
+    String name;
+String ders;
 
-    int idNum;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDers() {
+        return ders;
+    }
+
+    public void setDers(String ders) {
+        this.ders = ders;
+    }
 }
